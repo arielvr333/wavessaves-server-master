4a0272b1a5259b6de016359656067e2a7dcea92a
arielvr333 <arielvr333@gmail.com>
2022-04-02 12:05:18 +0300

    mongodb changes from tomer

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
 const udp = require('dgram');
-const { LEGAL_TCP_SOCKET_OPTIONS } = require('mongodb');
 const server = udp.createSocket('udp4');
+// const MongoClient = require('mongodb').MongoClient;
+// const url = "mongodb://0.0.0.0:27017";
+// const initializePassport = require('./passport-config');
+// const flash = require('express-flash');
+// const session = require('express-session');
+// const methodOverride = require('method-override')
+// const assert = require("assert");
 
+
+
+// app.use(methodOverride('_method'))
+// const users = [];
+// initializePassport(
+//     passport,
+//     UserName => users.find(user => user.UserName === UserName),
+//     id => users.find(user => user.id === id)
+// )
+// app.use(flash())
+// app.use(session({
+//     secret: process.env.SESSION_SECRET,
+//     resave: false,
+//     saveUninitialized: false
+// }))
+// app.use(passport.initialize())
+// app.use(passport.session())
 
 //mongo data base 
 // var MongoClient = require('mongodb').MongoClient;
-// var url = "mongodb://0.0.0.0:21771";
+// var url = "mongodb://127.0.0.1:27017";
 // MongoClient.connect(url, function (err, db) {
 //     if (err) throw err;
 //     var dbo = db.db("MongoDataBase");
@@ -34,8 +57,13 @@ server.on('message',function(msg,info){
             console.log('switch case login statement');
             login(splitedmessage[1],splitedmessage[2]);
             break;
+        case 'register':
+            console.log('switch case register statement');
+            register(splitMessage[1], splitMessage[2], info);
+            break;    
         case 'sensor':
-        console.log('switch case sensor input data statement');
+            sensor(splitedmessage[1],info.address,splitedmessage[2])
+            console.log('switch case sensor input data statement');
             
             default:
                 console.log('default statement');
@@ -53,14 +81,60 @@ server.on('message',function(msg,info){
     // if(info.address === "192.168.1.18")
     //     server.send("Alert",info.port,'192.168.1.18');
 });
-function login(username,password)
+function login(username,password,userip)
 {
+    // initializePassport(
+    //     passport,
+    //     UserName => users.find(user => user.UserName === UserName),
+    //     id => users.find(user => user.id === id)
+    // )
+    //     // update current user IP in DB
+    //     MongoClient.connect(url, function (err, db) {
+    //         assert.equal(null, err);
+    //         const dbo = db.db("WavesSavesDataBase");
+    //         dbo.collection('users').updateOne({"userName": username,}, {$set: {IP: userIP}}, function (err) {
+    //             assert.equal(null, err);
+    //         });
+    //     });
     console.log('username: ' +username);
     console.log('password: ' +password);
 }
-function sensor(sensorid,x,y,z)
+function register(username, password, info){
+    // const user = {
+    //     userName: username,
+    //     password: password,
+    //     IP: userIP
+    // };
+    // MongoClient.connect(url, function (err, db) {
+    //     assert.equal(null, err);
+    //     const dbo = db.db("WavesSavesDataBase");
+    //     const cursor = dbo.collection('users').find({userName: username});
+    //     // if exists : return not ok msg
+    //     // else insert and return ok
+    //     dbo.collection("users").insertOne(user, function (err, res) {
+    //         if (err) throw err;
+    //         server.send("OK", info.port, info.address)
+    //     });
+    // });
+    // console.log('username: ' +username);
+    // console.log('password: ' +password);
+}
+function sensor(sensorname,id,totaldata)
 {
-    
+    // MongoClient.connect(url, function (err, db) {
+    //     assert.equal(null, err);
+    //     const dbo = db.db("WavesSavesDataBase");
+    //     const cursor = dbo.collection('sensors').find({_id: info.address});
+    //     cursor.forEach(function (doc, err) {
+    //         for (let i = 0; i < doc.usersIP.length; i++)
+    //             sendAlert(doc.usersIP.get(i), info);
+    //     })
+    //  });
+}
+
+function sendAlert(userIP, info){
+    server.send("ALERT",info.port,userIP);
+    console.log('message sent to client: ' + userIP);
 }
 server.on('listening',function(){
     let address = server.address();
